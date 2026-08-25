9649970452bd1205f5d9e31c4beff3ed74a9852e
IgorAlexandre <igoralexandrevieira652@gmail.com>
2026-08-25 12:31:54 -0400

    feat: adicionado cloudinary provider para upload de arquivos e fotos.

--- config/plugins.js
+++ config/plugins.js
@@ -1,26 +1,7 @@
-const allowedMediaTypes = [
-  'image/*',
-  'video/*',
-  'audio/*',
-  'application/pdf',
-  'application/msword',
-  'application/vnd.openxmlformats-officedocument.*',
-  'text/plain',
-  'text/csv',
-];
-
-const deniedExecutableTypes = [
-  'application/vnd.microsoft.portable-executable',
-  'application/x-msdownload',
-  'application/x-msdos-program',
-  'application/x-executable',
-  'application/x-dosexec',
-  'application/x-sh',
-  'text/x-shellscript',
-  'application/x-mach-binary',
-];
-
-module.exports = () => ({
+/**
+ * @param {{ env: (key: string, defaultValue?: string) => string }} params
+ */
+module.exports = ({ env }) => ({
   'users-permissions': {
     config: {
       jwtManagement: 'refresh',
@@ -29,11 +10,45 @@ module.exports = () => ({
       },
     },
   },
+
   upload: {
     config: {
+      provider: 'cloudinary',
+
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+
       security: {
-        allowedTypes: allowedMediaTypes,
-        deniedTypes: deniedExecutableTypes,
+        allowedTypes: [
+          'image/*',
+          'video/*',
+          'audio/*',
+          'application/pdf',
+          'application/msword',
+          'application/vnd.openxmlformats-officedocument.*',
+          'text/plain',
+          'text/csv',
+        ],
+
+        deniedTypes: [
+          'application/vnd.microsoft.portable-executable',
+          'application/x-msdownload',
+          'application/x-msdos-program',
+          'application/x-executable',
+          'application/x-dosexec',
+          'application/x-sh',
+          'text/x-shellscript',
+          'application/x-mach-binary',
+        ],
+      },
+
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
